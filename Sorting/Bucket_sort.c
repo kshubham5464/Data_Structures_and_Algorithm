5fb86b761a8ad8b0f006cc1c202d72cfb62dc1d9
kshubham5464 <kshubham5462@gmail.com>
2025-04-03 23:17:44 +0530

    Updated feature

--- Sorting/Bucket_sort.c
+++ Sorting/Bucket_sort.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#define MAX 100    // Max elements
+#define BUCKETS 10 // Number of buckets
 
-#define MAX 100  // Max elements
-#define BUCKETS 10  // Number of buckets
-
-int main() {
+int main()
+{
     int arr[MAX], n, i, j, k;
     int bucket[BUCKETS][MAX], count[BUCKETS] = {0};
 
@@ -17,17 +17,21 @@ int main() {
         scanf("%d", &arr[i]);
 
     // Distribute elements into buckets
-    for (i = 0; i < n; i++) {
-        int index = arr[i] / BUCKETS;  // Find bucket index
+    for (i = 0; i < n; i++)
+    {
+        int index = arr[i] / BUCKETS; // Find bucket index
         bucket[index][count[index]++] = arr[i];
     }
 
     // Sort each bucket using simple sorting (Insertion Sort)
-    for (i = 0; i < BUCKETS; i++) {
-        for (j = 1; j < count[i]; j++) {
+    for (i = 0; i < BUCKETS; i++)
+    {
+        for (j = 1; j < count[i]; j++)
+        {
             int temp = bucket[i][j];
             k = j - 1;
-            while (k >= 0 && bucket[i][k] > temp) {
+            while (k >= 0 && bucket[i][k] > temp)
+            {
                 bucket[i][k + 1] = bucket[i][k];
                 k--;
             }
